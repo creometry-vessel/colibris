4363bcf27f262f7e947da837224068610161e85d
hadhemi-ouni <ounihadhami@gmail.com>
2021-10-07 20:07:42 +0100

    front designed
    Signed-off-by: hadhemi-ouni <ounihadhami@gmail.com>

--- front/src/App.js
+++ front/src/App.js
@@ -1,11 +1,13 @@
 import Markers from "./components/markers.component";
-import Form from "./components/form.component"
+import Form from "./components/form.component";
+import Home from './components/Home'
 import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 function App() {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" component={Form} />
+        <Route exact path="/" component={Home} />
+        <Route exact path ="/form" component={Form} />
         <Route exact path="/markers" component={Markers} />
       </Switch>
     </Router>
--- front/src/components/form.component.js
+++ front/src/components/form.component.js
@@ -42,6 +42,114 @@ export default function Form(){
     }
     return(
         <div>
+            <div className="wrapper">
+
+        <div id="content">
+            <header>
+                <div className="head-top">
+                    <div className="container">
+
+                        <div className="row">
+                            <div className="col-xl-4 col-lg-4 col-md-4 col-sm-4">
+                                <div className="email">
+                                    <a href="#"><img src="images/mail_icon.png" /> Email : colibris@gmail.com</a>
+                                </div>
+                            </div>
+                            <div className="col-xl-4 col-lg-4 col-md-4 col-sm-4">
+                                <div className="logo">
+                                    <a href="/"><img src="images/colibris.png" /></a>
+                                </div>
+                            </div>
+                            <div className="col-xl-4 col-lg-4 col-md-4 col-sm-4">
+                                <div className="contact_nu">
+                                    <a href="#"> <img src="images/phone_icon.png" /> Contact : +216 50 500 500</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="bg">
+                    <div className="container">
+                        <nav className="navigation navbar-expand-md  navbar-dark ">
+
+                            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
+                                <span className="navbar-toggler-icon"></span>
+                            </button>
+
+                        </nav>
+                    </div>
+                </div>
+            </header>
+            
+
+            <div id="contact" className="contact">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-12">
+                            <div className="title mt-5">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="container-fluid">
+
+                <div className="row">
+                    <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 padding">
+
+                        <form className="main_form">
+                            <div className="row">
+
+                                <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12">
+                                    <input className="form-control" placeholder="Nom et prénom" type="text" onChange={e=> setName(e.target.value)} value={name}/>
+                                </div>
+                                <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12">
+                                    <input className="form-control" placeholder="Numéro de téléphone" type="number" onChange={e=> setPhone(e.target.value)} value={phone}/>
+                                </div>
+                                <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12">
+                                    <input className="form-control" placeholder="Adresse" type="text" onChange={e=> setAddress(e.target.value)} value={address}/>
+                                </div>
+                                <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12">
+                                    <input className="form-control" placeholder="Latitude" type="text" onChange={e=> setlat(e.target.value)} type='number' value={lat} />
+                                </div>
+                                <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12">
+                                    <input className="form-control" placeholder="Longitude" type="text" onChange={e=> setlng(e.target.value)} type='number' value={lng} />
+                                </div>
+                                <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12">
+                                    <input type="button" value="submit" className="btn-main" onClick={Submit} />
+                                </div>
+                                <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 mt-2">
+                                    <button onClick={()=> window.location.href = "/markers"} className="btn-main" >{"Map-->"}</button>
+                                </div>
+
+                                
+                            </div>
+
+                        </form>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+    <footer>
+        <div className="footer">
+            <div className="copyright">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12">
+                            <p>© 2021 Colibris Designed by<a href="https://creometry.com"> Creometry</a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+        {/*
+
             <p>nom et prenom</p>
             <input placeholder="nom et prenom" onChange={e=> setName(e.target.value)} value={name} />
             <p>numero de tel</p>
@@ -56,7 +164,8 @@ export default function Form(){
             <input type="button" value="submit" onClick={Submit} />
             <br />
             <button onClick={()=> window.location.href = "/markers"}>{"Map-->"}</button>
-
+        
+        */}
         </div>
     )
 }
