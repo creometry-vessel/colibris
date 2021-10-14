f50037c7e410254907539c8a7a8d3c6f1ad64d3b
unknown <azizbenhadjyahia99@gmail.com>
2021-10-14 16:17:09 +0100

    modify client schema

--- back/models/client.models.js
+++ back/models/client.models.js
@@ -7,6 +7,11 @@ const clientSchema = new Schema({
         type: String, 
         required: true, 
     },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
     phone: {
         type: String,
         required: true,
