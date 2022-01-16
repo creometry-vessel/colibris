08a197950e073ae67886a4fc5ea7091680a89872
AzizBHY <62714614+Aziz-BHY@users.noreply.github.com>
2022-01-16 11:29:54 +0100

    Update config.js

--- front/public/config.js
+++ front/public/config.js
@@ -1,5 +1,4 @@
 window.ENV = {
     "USER_SERVICE_URI": "http://localhost:5001",
     "APPOINT_SERVICE_URI": "http://localhost:5000",
-    "GOOGLE_API_KEY": "AIzaSyBOqJFEL9f1qDYOZv9PPcEHOXGR0-V5vCU"
 }
