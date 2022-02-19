ce8a9eb05e284002236fb9762a9a68e40c465730
unknown <azizbenhadjyahia99@gmail.com>
2022-02-19 16:29:36 +0100

    merge feature-dashboard to dev

--- admin-ui/src/App.js
+++ admin-ui/src/App.js
@@ -1,18 +1,38 @@
 import ListAppointments from "./components/appointments.list";
 import Markers from "./components/markers.component";
 import { Route, HashRouter as Router, Switch } from "react-router-dom";
-
+import TopBar from "./components/TopBar";
+import Sidebar from "./components/Sidebar"
+import Users from "./components/Users";
 function App() {
   return (
-    
-    <Router hashType="noslash">
-      <Switch>
-        <Route exact path="/" component={ListAppointments} />
-        <Route exact path="/markers" component={Markers} />
-        
-      </Switch>
-    </Router>
-    
+    <div id="wrapper">
+      <TopBar />
+      <Sidebar />
+      
+      <div class="main">
+        <div class="main-content">
+          <div class="container-fluid">
+            {/* <ListAppointments /> */}
+            <Router hashType="noslash">
+              <Switch>
+                <Route
+                  exact
+                  path="/history"
+                  component={ListAppointments}
+                />
+                <Route
+                  exact
+                  path="/users"
+                  component={Users}
+                />
+                <Route exact path="/markers" component={Markers} />
+              </Switch>
+            </Router>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
