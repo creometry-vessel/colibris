cdc4168f7cdf281799435d6cf91dbf8eaff32879
unknown <azizbenhadjyahia99@gmail.com>
2022-04-15 03:28:36 +0000

    add sorting algorithm

--- schedule/routes/appointment.js
+++ schedule/routes/appointment.js
@@ -35,6 +35,56 @@ router.route('/').post(async(req, res) => {
     }
 
 })
+
+router.route("/sort").put(async (req,res)=>{
+    let locations = []
+    let destinations = ""
+    let indexes = [0];
+    let currentIndex = 0;
+    //fetch info
+    let appointments = await Appointment.find({shift: req.body.shift, dueDate: req.body.dueDate});
+    if(appointments.length == 0) {
+        res.json({data: "no Appointments today"})
+        return;
+    }
+    for (let appointment of appointments) {
+        let location = await axios.get(`${process.env.USER_SERVICE_URL}/location/${appointment.location}`);
+        destinations = `${destinations}|${location.data.address.lng},${location.data.address.lat}`
+        locations.push(location.data )
+    }
+
+    //send data to matrix API
+    let response = await axios.get(`https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=${destinations}&destinations=${destinations}&key=${process.env.GOOGLE_API_KEY}`);
+    //organize appointments
+    while(indexes.length != appointments.length){
+        let myDistance = 0 ;
+        let elements = response.data.rows[currentIndex].elements;
+        //get max distance
+        for(let k = 0; k <elements.length; k++){
+            if(elements[k].distance.value>= myDistance){
+                myDistance = elements[k].distance.value;
+            }
+        }
+        //search for min distance
+        let ind ;
+        for(let j = 0; j < elements.length; j++){
+            if(elements[j].distance.value <= myDistance && indexes.indexOf(j) == -1){
+                myDistance = elements[j].distance.value;
+                ind = j;
+            }
+        }
+        //go to next spot
+        indexes.push(ind)
+        currentIndex = ind;
+    }
+    //rank Appointments
+    for(let i = 0; i < indexes.length; i++){
+        appointments[i].waypointRank = indexes[i] + 1;
+        await appointments[i].save();
+    }
+    res.json({data: "succeeded"})
+})
+
 router.route("/").get(async(req, res) => {
     let filter = {};
     if (req.query.shift) filter.shift = req.query.shift;
@@ -108,4 +158,6 @@ router.route('/:id').put(async(req, res) => {
         res.json("Changed Successfully !")
     } else res.json("already booked")
 })
+
+
 module.exports = router;
