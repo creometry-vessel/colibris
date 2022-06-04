0c332b1a96df79b5fe554a2cff4fb11cccba8339
hadhemi-ouni <ounihadhami@gmail.com>
2022-06-04 17:32:26 +0100

    initial commit
    Signed-off-by: hadhemi-ouni <ounihadhami@gmail.com>

---
+++ admin-ui/public/config.js
@@ -0,0 +1,6 @@
+window.ENV = {
+    "APPOINT_SERVICE_URI": "http://localhost:5000",
+    "USER_SERVICE_URI": "http://localhost:5001",
+    "GOOGLE_API_KEY": "AIzaSyCZFzpJI33hkolnb_bU-N9HlvmiGK54VRo",
+    "ZONE_SERVICE_URI": "http://localhost:5002/zone",
+}
