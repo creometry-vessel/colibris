d40ae0232d8de7d2933a1c275de2bb3a13f0de83
AzizBHY <62714614+Aziz-BHY@users.noreply.github.com>
2022-06-13 13:17:14 +0100

    control avatar form

--- customer-ui/src/components/dialogEdit.js
+++ customer-ui/src/components/dialogEdit.js
@@ -1,10 +1,42 @@
-import * as React from 'react';
 import Dialog from '@mui/material/Dialog';
-
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { useCookies } from "react-cookie";
 export default function EditDialog(props) {
+  const [cookies] = useCookies(["colibrisID"]);
+  const [avatar, setAvatar] = useState("")
     const handleClose = () => {
       props.setOpen(false);
     };
+    const Submit = ()=>{
+      fetch("config/USER_SERVICE_URI")
+      .then((r) => r.text())
+      .then((USER_SERVICE_URI) => {
+        axios
+          .put(`${USER_SERVICE_URI}/${cookies.colibrisID}`, {avatar: avatar})
+          .then((res) => {
+            console.log(res)
+            handleClose()
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      });
+    }
+    useEffect(()=>{
+      fetch("config/USER_SERVICE_URI")
+      .then((r) => r.text())
+      .then((USER_SERVICE_URI) => {
+        axios
+          .get(`${USER_SERVICE_URI}/${cookies.colibrisID}`)
+          .then((res) => {
+            setAvatar(res.data.avatar)
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      });
+    }, [])
   return (
     
     <Dialog onClose={handleClose} fullWidth={true} maxWidth={"md"} open={props.openDialog}>
@@ -15,8 +47,10 @@ export default function EditDialog(props) {
             placeholder="Lien vers la photo"
             className="form-control mt-3"
             type="text"
-            
+            value={avatar}
+            onChange={(e)=>setAvatar(e.target.value)}
           />
+          <button className='btn custom-btn mt-4' onClick={Submit}>Submit</button>
       </div>
       </div>
     </Dialog>
